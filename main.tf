87092682b2a07fbb0fddcd52f95dec5a5b251314
Randeep singh <jazzysingh227@gmail.com>
2025-05-15 21:10:12 +0530

    new files added

--- main.tf
+++ main.tf
@@ -1,22 +1,6 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "6.0.0-beta1"
-    }
-  }
-}
-
 provider "aws" {
-  region= "ap-south-1"
+  region= var.aws_region
 }
 
-resource "aws_instance" "terra-ghost" {
-  ami           = "ami-0e35ddab05955cf57"
-  instance_type = "t2.micro"
 
-  tags = {
-    Name = "terraghost"
-  }
-}
 
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  description = "Value for aws region"
+}
+
+variable "aws_ami"{
+  description = "Value for ami"
+}
+
+variable "aws_instance_type" {
+  description = "Value for instance type"
+}
+
+variable "aws_instance_tag" {
+  description = "Value for tag"
+}
